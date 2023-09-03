7eb690769fb0e02904e0f8ebe84f86eab52d5a7b
Amritha <amrithapriya.praveen@gmail.com>
2023-09-02 21:40:08 -0500

    Initial Commit

---
+++ IJAS/IJASApp.swift
@@ -0,0 +1,17 @@
+//
+//  IJASApp.swift
+//  IJAS
+//
+//  Created by Krithik Praveen on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct IJASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
