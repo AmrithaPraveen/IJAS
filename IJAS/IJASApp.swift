f0888b58f2764d46d4bf7f4d9956f86ee7fd8ecf
basnetSantosh <santro_zing00@yahoo.com>
2023-09-09 15:50:01 -0400

    making firebase connection

--- IJAS/IJASApp.swift
+++ IJAS/IJASApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct IJASApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             MainTabbedView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
